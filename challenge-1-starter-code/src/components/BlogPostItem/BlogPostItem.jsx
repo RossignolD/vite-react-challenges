df81fc4de946f9d3675acda5b848fadc1ec3371b
RossignolD <knightsoflogicland@gmail.com>
2025-04-08 16:16:42 -0400

    css for challenge 3 now working somewhat, at least the css reset is working now

--- challenge-1-starter-code/src/components/BlogPostItem/BlogPostItem.jsx
+++ challenge-1-starter-code/src/components/BlogPostItem/BlogPostItem.jsx
@@ -1,13 +1,15 @@
 // src/App.jsx
-import React from 'react';
-import styles from './BlogPostItem.module.css'; 
+import React from "react";
+import "../../reset.css";
+import styles from "./BlogPostItem.module.css";
 import { Link } from "react-router";
 
-
-function BlogPostItem({title, summary, date, url}) {
+function BlogPostItem({ title, summary, date, url }) {
   return (
     <div className={styles.blogPostItem}>
-      <Link to={url} className={styles.title}><h2>{title}</h2> </Link>
+      <Link to={url} className={styles.title}>
+        <h2>{title}</h2>{" "}
+      </Link>
       <p className={styles.summary}>{summary}</p>
 
       <p className={styles.date}>Published on {date}</p>
